eb103d6f5e528576f0b62ceb43e05a3dec2be53d
rohu3 <69983280+rohu3@users.noreply.github.com>
2020-08-29 20:06:46 +0100

    Add files via upload
    Here is my project , Academind replication so far - learning GitHub

---
+++ #Replication from Scratch/contact/side.js
@@ -0,0 +1,39 @@
+var iname = document.querySelector(".iname");
+var email = document.querySelector(".email");
+var message = document.querySelector(".message");
+var button = document.querySelector(".button");
+
+
+
+
+function formValidation() {
+
+
+
+}
+
+function formValidation() {
+    if (iname.value!== "" ) {
+        console.log(name.value);
+        button.style.backgroundColor = 'red';
+        
+    } else {
+        button.style.backgroundColor = 'green';
+       
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
